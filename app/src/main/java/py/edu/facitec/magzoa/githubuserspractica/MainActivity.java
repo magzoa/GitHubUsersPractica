044aefbaa8d89926ed83e219968f4195f15aaf6c
magzoa <magzoaa@gmail.com>
2025-09-16 10:07:10 -0300

    Video 3

--- app/src/main/java/py/edu/facitec/magzoa/githubuserspractica/MainActivity.java
+++ app/src/main/java/py/edu/facitec/magzoa/githubuserspractica/MainActivity.java
@@ -2,6 +2,11 @@ package py.edu.facitec.magzoa.githubuserspractica;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit.Callback;
@@ -17,6 +23,11 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity implements Callback<List<User>> {
+
+    ProgressBar progressBar;
+    ListView usersListView;
+    TextView errorTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +39,15 @@ public class MainActivity extends AppCompatActivity implements Callback<List<Use
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        progressBar=findViewById(R.id.progressBar);
+        usersListView=findViewById(R.id.listViewUsers);
+        errorTextView=findViewById(R.id.textViewError);
+
+
+
+
+
 
 
         RestAdapter restAdapter=new RestAdapter.Builder()
@@ -46,12 +66,29 @@ public class MainActivity extends AppCompatActivity implements Callback<List<Use
     @Override
     public void success(List<User> users, Response response) {
 
-        Log.i("GITHUB", users.toString());
+       // Log.i("GITHUB", users.toString());
+
+        progressBar.setVisibility(View.GONE);
+
+        usersListView.setVisibility(View.VISIBLE);
+
+
+
+        ArrayAdapter<String> adapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1,users);
+
+        usersListView.setAdapter(adapter);
+
+
 
     }
 
     @Override
     public void failure(RetrofitError error) {
-Log.e("GITHUB",error.getLocalizedMessage());
+        //Log.e("GITHUB",error.getLocalizedMessage());
+    progressBar.setVisibility(View.GONE);
+    errorTextView.setVisibility(View.VISIBLE);
+    errorTextView.setText(getString(R.string.error_msj)+"\n"+error.getLocalizedMessage());
+
+
     }
 }
